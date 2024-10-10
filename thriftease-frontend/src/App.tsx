22ff19976f0cda27249d2c496d0a8e8eac893225
Oderinde Michael <Olubiyioderinde7@gmail.com>
2024-10-10 00:26:38 -0700

    "Updated App.tsx with new routes and components, modified Login.tsx and Signup.tsx, added icons to SideNav.tsx, and made changes to Home.tsx, HomeBody.tsx, and index.css."

--- thriftease-frontend/src/App.tsx
+++ thriftease-frontend/src/App.tsx
@@ -6,12 +6,25 @@ import Intro from "./components/Intro";
 import Home from "./components/home/Home";
 import Login from "./components/Auth/Login";
 import Signup from "./components/Auth/Signup";
+import HomeBody from "./components/home/HomeBody";
+import DashBoard from "./components/DashBoard/DashBoard";
+import SellAnItem from "./components/Listing.tsx/SellAnItem";
+import Cart from "./components/Cart/Cart";
+import Category from "./components/Category/Category";
+import Search from "./components/Search/Search";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Intro />} />
-      <Route path="home" element={<Home />} />
+      <Route path="home" element={<Home />}>
+        <Route index element={<HomeBody />} />
+        <Route path="dashboard" element={<DashBoard />} />
+        <Route path="listing" element={<SellAnItem />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="category" element={<Category />} />
+        <Route path="search" element={<Search />} />
+      </Route>
       <Route path="login" element={<Login />} />
       <Route path="register" element={<Signup />} />
     </Routes>
